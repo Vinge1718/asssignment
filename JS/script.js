68618969d299c374c075635658da8cd950ac61f2
Vinge1718 <vinge1718@gmail.com>
2017-08-11 16:18:57 +0300

    declare the varibles that are going to hold the places for customer details

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,10 @@
-$(document).ready(function(event){
-  event.preventDefault();
+$(document).ready(function(){
+  $("#detailsSubmitButton button").click(function(event){
+    var firstName = $("input#firstName").val();
+    var secondName= $("input#secondName").val();
+    var shippingAddress = $("#shippingAddress").val();
+    var gender = $("input:radio[name=gender]:checked").val();
+    var contact = $("input#emailAddress").val();
+    event.preventDefault();
+  });
 });
